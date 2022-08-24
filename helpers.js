b4fecf8e99b065820ef96d56dd7a7bbbde646302
romero8 <101278601+romero8@users.noreply.github.com>
2022-08-24 23:32:28 +0300

    Delete helpers.js

--- helpers.js
+++
@@ -1,7 +0,0 @@
-export function getRandomNumber(min, max) {
-    let step1 = max - min + 1;
-    let step2 = Math.random() * step1;
-    let result = Math.floor(step2) + min;
-  
-    return result;
-  }
